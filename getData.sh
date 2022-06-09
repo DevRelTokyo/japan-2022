4549da64cd05e6936db85fd224958dd527063a54
Atsushi <atsushi@moongift.jp>
2022-06-09 16:22:37 +0900

    Add sponsors

--- getData.sh
+++ getData.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE3syN3fDTXJlW6gis8DYmQuhjcGG0QMNgQQ/exec?name=organizers -O _data/organizers.json 
+wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE3syN3fDTXJlW6gis8DYmQuhjcGG0QMNgQQ/exec?name=sponsors -O _data/sponsors.json
