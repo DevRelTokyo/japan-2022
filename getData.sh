2c020efd6e0b9d8555741c8eff78fdebbe66d1e9
Atsushi <atsushi@moongift.jp>
2022-08-04 16:23:17 +0900

    add lts

--- getData.sh
+++ getData.sh
@@ -5,5 +5,6 @@ wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE
 wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE3syN3fDTXJlW6gis8DYmQuhjcGG0QMNgQQ/exec?name=sessions -O _data/sessions.json
 wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE3syN3fDTXJlW6gis8DYmQuhjcGG0QMNgQQ/exec?name=tracks -O _data/tracks.json
 wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE3syN3fDTXJlW6gis8DYmQuhjcGG0QMNgQQ/exec?name=venues -O _data/venues.json
+wget https://script.google.com/macros/s/AKfycbzQO8oVb8MFJ1th0HSpUBcYX4y26erPkpoE3syN3fDTXJlW6gis8DYmQuhjcGG0QMNgQQ/exec?name=lts -O _data/lts.json
 ruby speaker.rb
 ruby session.rb
